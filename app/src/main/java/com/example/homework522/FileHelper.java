bcb9f7fa66c5910f58d1316ae2fe82b935144236
NatalyVolegova <52189961+natvolegova@users.noreply.github.com>
2019-08-20 14:17:16 +0300

    Update FileHelper.java

--- app/src/main/java/com/example/homework522/FileHelper.java
+++ app/src/main/java/com/example/homework522/FileHelper.java
@@ -1,33 +1,47 @@
 package com.example.homework522;
 
-import android.content.Context;
 
-import java.io.BufferedInputStream;
+import android.content.Context;
+import android.os.Environment;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-
-import static android.content.Context.MODE_PRIVATE;
 
 public class FileHelper {
     private Context context;
-    private String fileName;
 
-    public FileHelper(Context context, String fileName) {
+
+    public FileHelper(Context context) {
         this.context = context;
-        this.fileName = fileName;
-        //создаем системный файл для хранения данных
+    }
+
+    //создаем файл во внутреннем хранилище
+    public void createFileRegInternal(String fileName) {
+        String actualFile = context.getFilesDir() + "/" + fileName;
+        createFile(actualFile);
+    }
+
+    //создаем файл во внешнем хранилище
+    public void createFileRegExternal(String fileName) {
+        if (isExternalStorageWritable()) {
+            String actualFile = context.getApplicationContext().getExternalFilesDir(null) + "/" + fileName;
+            createFile(actualFile);
+        } else {
+            Toast.makeText(context, "File Error", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void createFile(String fileName) {
         try {
-            File file = new File(fileName);
-            if (!file.exists()) {
-                file.createNewFile();
+            File fileSetting = new File(fileName);
+            if (!fileSetting.exists()) {
+                fileSetting.createNewFile();
             }
         } catch (NullPointerException e) {
             e.printStackTrace();
@@ -36,8 +50,8 @@ public class FileHelper {
         }
     }
 
-    //обновляем данные
-    public void updateValue(String login, String pass) {
+    //обновляем данные во внутреннем хранилище
+    public void updateInternalValue(String fileName, String login, String pass) {
         FileOutputStream outputStream;
         String value = login + "\n" + pass;
         try {
@@ -49,8 +63,21 @@ public class FileHelper {
         }
     }
 
-    //получаем данные из файла
-    public String getValue() {
+    //обновляем данные во внешнем хранилище
+    public void updateExternalValue(String fileName, String login, String pass) {
+        String actualFile = context.getApplicationContext().getExternalFilesDir(null) + "/" + fileName;
+        String value = login + "\n" + pass;
+        try {
+            FileWriter textFileWriter = new FileWriter(actualFile, true);
+            textFileWriter.write(value);
+            textFileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //получаем данные из файла из внутреннего хранилища
+    public String getInternalValue(String fileName) {
         FileInputStream fis = null;
         StringBuilder output = new StringBuilder();
         try {
@@ -76,5 +103,64 @@ public class FileHelper {
             }
         }
         return output.toString();
+    }
+
+    //получаем данные из файла из внешнего хранилища
+    public String getExternalValue(String fileName) {
+        String actualFile = context.getApplicationContext().getExternalFilesDir(null) + "/" + fileName;
+        File fileReg = new File(actualFile);
+        FileInputStream fis = null;
+        StringBuilder output = new StringBuilder();
+        try {
+            fis = new FileInputStream(fileReg);
+            InputStreamReader inputStreamReader = new InputStreamReader(fis);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = "";
+            // читаем содержимое по строкам
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(";");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }
+        return output.toString();
+    }
+
+    //удаляем файл во внутреннем хранилище
+    public void deleteInternalFile(String fileName) {
+        try {
+            String actualFile = context.getFilesDir() + "/" + fileName;
+            new File(actualFile).delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //удаляем файл во внешнем хранилище
+    public void deleteExternalFile(String fileName) {
+        if (isExternalStorageWritable()) {
+            String actualFile = context.getApplicationContext().getExternalFilesDir(null) + "/" + fileName;
+            new File(actualFile).delete();
+        } else {
+            Toast.makeText(context, "File Error", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
