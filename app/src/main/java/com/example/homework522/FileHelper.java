95bf30857cfb04dec8c3dcfd9d966a3604927d50
NatalyVolegova <info@volegova.ru>
2019-08-23 10:47:30 +0400

    Changes in AppConfig

--- app/src/main/java/com/example/homework522/FileHelper.java
+++ app/src/main/java/com/example/homework522/FileHelper.java
@@ -1,71 +1,91 @@
 package com.example.homework522;
 
-import android.content.Context;
 
-import java.io.BufferedInputStream;
+import android.content.Context;
+import android.os.Environment;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class FileHelper {
     private Context context;
-    private String fileName;
 
-    public FileHelper(Context context, String fileName) {
+
+    public FileHelper(Context context) {
         this.context = context;
-        this.fileName = fileName;
-        //создаем системный файл для хранения данных
-        try {
-            File file = new File(fileName);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
-    //обновляем данные
-    public void updateValue(String login, String pass) {
-        FileOutputStream outputStream;
+
+    //обновляем данные во внутреннем хранилище
+    public void updateInternalValue(String fileName, String login, String pass) {
+        FileOutputStream fos = null;
         String value = login + "\n" + pass;
         try {
-            outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
-            outputStream.write(value.getBytes());
-            outputStream.close();
-        } catch (Exception e) {
+            fos = context.openFileOutput(fileName, MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            bw.write(value);
+            bw.close();
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException ignored) {
+                    ignored.printStackTrace();
+                }
+            }
         }
     }
 
-    //получаем данные из файла
-    public String getValue() {
+    //обновляем данные во внешнем хранилище
+    public void updateExternalValue(String fileName, String login, String pass) {
+        String actualFile = context.getApplicationContext().getExternalFilesDir(null) + "/" + fileName;
+        String value = login + "\n" + pass;
+        FileWriter textFileWriter = null;
+        try {
+            textFileWriter = new FileWriter(actualFile, true);
+            textFileWriter.write(value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (textFileWriter != null) {
+                try {
+                    textFileWriter.close();
+                } catch (IOException ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //получаем данные из файла из внутреннего хранилища
+    public String getInternalValue(String fileName) {
         FileInputStream fis = null;
         StringBuilder output = new StringBuilder();
+        String line = "";
         try {
             fis = context.openFileInput(fileName);
             InputStreamReader inputStreamReader = new InputStreamReader(fis);
             BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = "";
             // читаем содержимое по строкам
             while ((line = reader.readLine()) != null) {
                 output.append(line).append(";");
             }
-
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            return line;
         } finally {
             if (fis != null) {
                 try {
@@ -76,5 +96,64 @@ public class FileHelper {
             }
         }
         return output.toString();
+    }
+
+    //получаем данные из файла из внешнего хранилища
+    public String getExternalValue(String fileName) {
+        String actualFile = context.getApplicationContext().getExternalFilesDir(null) + "/" + fileName;
+        File fileReg = new File(actualFile);
+        FileInputStream fis = null;
+        StringBuilder output = new StringBuilder();
+        String line = "";
+        try {
+            fis = new FileInputStream(fileReg);
+            InputStreamReader inputStreamReader = new InputStreamReader(fis);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            // читаем содержимое по строкам
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(";");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return line;
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }
+        return output.toString();
+    }
+
+    //удаляем файл во внутреннем хранилище
+    public void deleteInternalFile(String fileName) {
+        try {
+            String actualFile = context.getFilesDir() + "/" + fileName;
+            new File(actualFile).delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //удаляем файл во внешнем хранилище
+    public void deleteExternalFile(String fileName) {
+        if (isExternalStorageWritable()) {
+            String actualFile = context.getApplicationContext().getExternalFilesDir(null) + "/" + fileName;
+            new File(actualFile).delete();
+        } else {
+            Toast.makeText(context, context.getResources().getString(R.string.msg_file_error), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/homework522/SignInActivity.java
+++ app/src/main/java/com/example/homework522/SignInActivity.java
@@ -1,9 +1,14 @@
 package com.example.homework522;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -13,7 +18,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignInActivity extends AppCompatActivity {
     private Button btnSignin;
     private Button btnSignup;
     private TextView txtSignup;
@@ -22,17 +27,118 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private LinearLayout layoutSignup;
     private FileHelper fh;
     private AppConfig config;
+    private Intent intent;
+    private String filename;
+    private Boolean curStorage;
+    private EditText etLogin;
+    private EditText etPass;
+    private EditText etLoginUp;
+    private EditText etPassUp;
 
-    public static final String FILE_REG = "txt_reginfo.txt";
+    public static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 11; //пользовательская переменная, определяем права доступа на запись
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AppConfig config = new AppConfig(SignInActivity.this);
+        config = new AppConfig(SignInActivity.this);
         config.createMainConfig();
-        config.initBaseConfig();
-        setContentView(R.layout.activity_sign_in);
-        initView();
+        fh = new FileHelper(SignInActivity.this);
+        intent = new Intent(SignInActivity.this, MainActivity.class);
+
+        //проверяем, авторизован ли пользователь
+        if (config.isLoginned()) {
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.activity_sign_in);
+            initView();
+
+            filename = config.getFileSetting();
+            curStorage = config.getStorage();
+
+            //авторизация
+            btnSignin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String login = etLogin.getText().toString();
+                    String pass = etPass.getText().toString();
+                    String curData = "";
+                    if (!login.equals("") && !pass.equals("")) {
+                        //получаем регистрационные данные из файла, проверяем где находится файл регистрации
+                        if (curStorage) {
+                            //внешнее хранилище
+                            curData = fh.getExternalValue(filename);
+                        } else {
+                            //внутреннее хранилище
+                            curData = fh.getInternalValue(filename);
+                        }
+                        //проверяем, есть ли файл с данными и пользователь ранее регистрировался
+                        if (!curData.equals("")) {
+                            String[] curValue = curData.split(";");
+                            //проверяем введенные данные и сравниваем их с данными файла
+                            if (login.equals(curValue[0]) && pass.equals(curValue[1])) {
+                                startActivity(intent);
+                                config.setLoginned(true);
+                            } else {
+                                Toast.makeText(SignInActivity.this, getResources().getString(R.string.msg_error_signin), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            //учетная запись не найдена
+                            Toast.makeText(SignInActivity.this, getResources().getString(R.string.msg_error_noregister), Toast.LENGTH_LONG).show();
+                        }
+                        etLogin.setText("");
+                        etPass.setText("");
+                    } else {
+                        Toast.makeText(SignInActivity.this, getResources().getString(R.string.msg_error_signup), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+            //регистрация
+            btnSignup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String loginUp = etLoginUp.getText().toString();
+                    String passUp = etPassUp.getText().toString();
+                    if (!loginUp.equals("") && !passUp.equals("")) {
+                        if (curStorage) {
+                            //внешнее хранилище
+                            int permissionStatus = ContextCompat.checkSelfPermission(SignInActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                            if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+                                // fh.createFileRegExternal(filename);
+                                fh.updateExternalValue(filename, loginUp, passUp);
+                            } else {
+                                ActivityCompat.requestPermissions(SignInActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_WRITE_STORAGE);
+                            }
+                        } else {
+                            //внутреннее хранилище
+                            fh.updateInternalValue(filename, loginUp, passUp);
+                        }
+                        config.setLoginned(true);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(SignInActivity.this, getResources().getString(R.string.msg_error_signup), Toast.LENGTH_LONG).show();
+                    }
+                    etLoginUp.setText("");
+                    etPassUp.setText("");
+                }
+            });
+            //ссылка для открытия layout регистрации
+            txtSignup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    layoutSignup.setVisibility(View.VISIBLE);
+                    layoutSignin.setVisibility(View.GONE);
+                }
+            });
+            //ссылка для открытия layout авторизации
+            txtSignin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    layoutSignin.setVisibility(View.VISIBLE);
+                    layoutSignup.setVisibility(View.GONE);
+                }
+            });
+
+        }
     }
 
     private void initView() {
@@ -40,77 +146,23 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         btnSignup = findViewById(R.id.btn_signup);
         txtSignup = findViewById(R.id.txt_signup);
         txtSignin = findViewById(R.id.txt_signin);
-        btnSignin.setOnClickListener(this);
-        btnSignup.setOnClickListener(this);
-        txtSignup.setOnClickListener(this);
-        txtSignin.setOnClickListener(this);
         layoutSignin = findViewById(R.id.layout_signin);
         layoutSignup = findViewById(R.id.layout_signup);
-
-        config = new AppConfig(SignInActivity.this);
-        Boolean curStorage = config.getStorage();
-        String fileDir="";
-        if(curStorage){
-            //внешнее хранилище
-            fileDir = getApplicationContext().getExternalFilesDir(null)+"/";
-        }else{
-            //внутреннее хранилище
-            fileDir = getFilesDir()+"/";
-        }
-        // полный путь к файлу
-        String actualFile = fileDir + FILE_REG;
-        Toast.makeText(SignInActivity.this,actualFile, Toast.LENGTH_SHORT).show();
-        fh = new FileHelper(SignInActivity.this, actualFile);
+        etLogin = findViewById(R.id.et_login);
+        etPass = findViewById(R.id.et_pass);
+        etLoginUp = findViewById(R.id.et_login_up);
+        etPassUp = findViewById(R.id.et_pass_up);
     }
 
     @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-        switch (view.getId()) {
-            //авторизация
-            case R.id.btn_signin:
-                EditText etLogin = findViewById(R.id.et_login);
-                EditText etPass = findViewById(R.id.et_pass);
-                String login = etLogin.getText().toString();
-                String pass = etPass.getText().toString();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION_WRITE_STORAGE: //пользовательская переменная, доступ на запись
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                if (!login.equals("") && !pass.equals("")) {
-                    //получаем регистрационные данные из файла
-                    String[] curvalue = fh.getValue().split(";");
-                    if (login.equals(curvalue[0]) && pass.equals(curvalue[1])) {
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(SignInActivity.this, getResources().getString(R.string.msg_error_signin), Toast.LENGTH_LONG).show();
-                    }
-                    etLogin.setText("");
-                    etPass.setText("");
                 } else {
-                    Toast.makeText(SignInActivity.this, getResources().getString(R.string.msg_error_signup), Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, getResources().getString(R.string.msg_request_denied), Toast.LENGTH_LONG).show();
                 }
-                break;
-            //регистрация
-            case R.id.btn_signup:
-                EditText etLoginUp = findViewById(R.id.et_login_up);
-                EditText etPassUp = findViewById(R.id.et_pass_up);
-                String loginup = etLoginUp.getText().toString();
-                String passup = etPassUp.getText().toString();
-                if (!loginup.equals("") && !passup.equals("")) {
-                    fh.updateValue(loginup, passup);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(SignInActivity.this, getResources().getString(R.string.msg_error_signup), Toast.LENGTH_LONG).show();
-                }
-                etLoginUp.setText("");
-                etPassUp.setText("");
-                break;
-            //переключаем экраны для показа формы авторизации или регистрации
-            case R.id.txt_signup:
-                layoutSignup.setVisibility(View.VISIBLE);
-                layoutSignin.setVisibility(View.GONE);
-                break;
-            case R.id.txt_signin:
-                layoutSignin.setVisibility(View.VISIBLE);
-                layoutSignup.setVisibility(View.GONE);
                 break;
         }
     }
